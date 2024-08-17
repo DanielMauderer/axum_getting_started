675c18e0cd0c655f7303ec5ffaa037dbf538a89b
Daniel Mauderer <daniel.mauderer@outlook.com>
2024-08-17 22:44:38 +0200

    init todo app

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,14 @@
-// rust axum main function
-async fn main() {}
+// this will be a to do app
+
+use std::env;
+
+// todo <command> <rest>
+// todo <add> <todo-name> <description>
+// todo <edit> <todo-name> <description>
+// todo <tick> <todo-name>
+// todo <remove> <todo-name>
+// todo <list>
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+}
